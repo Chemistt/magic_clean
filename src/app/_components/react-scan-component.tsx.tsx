718ca9921650612255a624085b69c2cd0fa99e40
EEFREY <eefrey.10@gmail.com>
2025-04-19 03:47:09 +0800

    react-scan must be imported before react - for what

--- src/app/_components/react-scan-component.tsx.tsx
+++ src/app/_components/react-scan-component.tsx.tsx
@@ -1,14 +1,15 @@
 "use client";
 // react-scan must be imported before react
-import { type JSX, useEffect } from "react";
+// eslint-disable-next-line simple-import-sort/imports
 import { scan } from "react-scan";
+import { type JSX, useEffect } from "react";
 
 export function ReactScan(): JSX.Element {
-  useEffect(() => {
-    scan({
-      enabled: true,
-    });
-  }, []);
+	useEffect(() => {
+		scan({
+			enabled: true,
+		});
+	}, []);
 
-  return <></>;
+	return <></>;
 }
